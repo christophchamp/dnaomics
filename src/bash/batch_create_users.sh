01a4f1263ecb73dc2cc201ba83c6fe1b925f5cb0
Christoph Champ <christoph.champ@gmail.com>
2015-04-26 21:03:18 -0500

    a better method

--- src/bash/batch_create_users.sh
+++ src/bash/batch_create_users.sh
@@ -9,7 +9,7 @@
 # 3. The script must create a system user for each user listed in the input
 #    file, with a password of the username reversed.
 # Example user list file:
-# $ cat users.txt
+# $ cat /path/to/users.txt
 # alice   Bill,charlie;danny
 # earl
 # frank;greg
@@ -18,6 +18,8 @@
 # password of "ecila", a user "Bill" with the password "lliB", etc.
 
 usage() { echo "usage: $0 [-f <path_to_file>]" 1>&2; exit 1; }
+
+USERADD=/usr/sbin/useradd
 
 while getopts ":f:" o; do
     case "${o}" in
@@ -33,12 +35,13 @@ shift $((OPTIND-1))
 
 for username in `sed -e :a -e '$!N;s/\n/ /;ta' -e 's/[ ;,]\{1,\}/ /g' ${path}`; do
     password=`echo "${username}"|rev`;
-    echo "NAME=${username}; PASSWORD=${password}";
-    sudo useradd ${username}
+    #echo "NAME=${username}; PASSWORD=${password}";
+    #sudo $USERADD ${username}
 
     # Change/add password to new user
     # NOTE: `--stdin` has been deprecated on newer distros. Use `chpasswd` on those
     # sudo echo 'username:password' | chpasswd
     # sudo echo -e "password\npassword\n" | passwd
-    sudo echo ${password}|passwd ${username} --stdin
+    #sudo echo ${password}|passwd ${username} --stdin
+    $USERADD -p $(openssl passwd -1 ${password}) ${username}
 done
