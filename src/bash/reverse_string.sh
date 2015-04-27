a58fa9b3d0bfed64e5d633d3a844ba976d92a19f
Christoph Champ <christoph.champ@gmail.com>
2015-04-27 18:50:44 -0500

    python/php examples

--- src/bash/reverse_string.sh
+++ src/bash/reverse_string.sh
@@ -3,8 +3,12 @@
 #
 # $ echo "Alice" | rev
 # $ rev<<<"Alice"
+# $ echo Alice | python -c 'import sys;print(sys.stdin.read().strip()[::-1])'
+# $ python -c 'import sys;print(sys.stdin.read().strip()[::-1])' <<< Alice
 # $ perl -ne 'chomp;print scalar reverse . "\n";'<<<"Alice"
 # $ echo 'Alice' | perl -ne 'chomp;print scalar reverse . "\n";'
+# $ echo Alice | php -r 'print strrev(trim(fgets(STDIN)));'
+# $ php -r 'print strrev(trim(fgets(STDIN)));' <<< Alice
 
 input="$1"
 reverse=""
